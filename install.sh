3ad6dc35c9e570f64655c0c693a0b0aa257efc7c
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2025-03-26 20:28:23 +0100

    added real links to scripts

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ mkdir "$CI_TEMPLATE_DIR"
 wget -O "$CI_TEMPLATE_DIR" "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.raw"
 
 mkdir "$CI_SCRIPTS"
-wget -O "$CI_SCRIPTS/debian12.sh" "TODO"
+wget -O "$CI_SCRIPTS/debian12.sh" "https://raw.githubusercontent.com/ZaptoInc/cloudinit/refs/heads/master/scripts/debian12.sh"
 
-wget -O "/usr/local/bin/cloudinit" "TODO"
+wget -O "/usr/local/bin/cloudinit" "https://raw.githubusercontent.com/ZaptoInc/cloudinit/refs/heads/master/cloudinit"
 
 echo "TODO"
