ec54df30d160c39196c5c30d2d866eb1b1fadd97
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2025-03-26 22:36:06 +0100

    default OS and os related argument parsing

--- variables.sh
+++ variables.sh
@@ -10,6 +10,8 @@ export CI_CIDR=24
 export CI_GATEWAY="192.168.1.1"
 export CI_DNS="1.1.1.1 1.0.0.1"
 
+export CI_OS="debian12"
+
 export CI_ROOT_PASSWORD="root"
 export CI_USER_NAME="user"
 export CI_USER_PASSWORD="user"
@@ -17,6 +19,8 @@ export CI_USER_PASSWORD="user"
 export CI_CPU=1
 export CI_RAM=2048
 export CI_DISK="20G"
+
+export CI_STORAGE="local-lvm"
 
 CI_CUSTOM_VARIABLES="/etc/cloudinit/custom.sh"
 if [[ -e $CI_CUSTOM_VARIABLES ]]; then
