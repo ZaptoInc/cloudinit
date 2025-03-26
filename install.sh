dd48474b34a5454202595905cf03e8ccb0bc5140
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2025-03-26 21:36:28 +0100

    update...

--- install.sh
+++ install.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
+export CI_VERBOSE=1
+
 # Downloading variables config file
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Creating /etc/cloudinit if it doesn't exist"
+fi
 mkdir -p "/etc/cloudinit"
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Downloading latest variable.sh to /etc/cloudinit..."
+fi
 wget -O "/etc/cloudinit/variables.sh" "https://raw.githubusercontent.com/ZaptoInc/cloudinit/refs/heads/master/variables.sh"
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Making it executable"
+fi
 chmod +x "/etc/cloudinit/variables.sh"
 
 # Importing variables config
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Importing variables"
+fi
 source "/etc/cloudinit/variables.sh"
 
+# Creating main directory
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Creating $CI_MAIN_DIR if it doesn't exist"
+fi
+mkdir -p "$CI_MAIN_DIR"
+
 # Importing OS scripts
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Creating $CI_SCRIPTS if it doesn't exist"
+fi
 mkdir -p "$CI_SCRIPTS"
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Downloading latest debian12.sh to $CI_SCRIPTS..."
+fi
 wget -O "$CI_SCRIPTS/debian12.sh" "https://raw.githubusercontent.com/ZaptoInc/cloudinit/refs/heads/master/scripts/debian12.sh"
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Making it executable"
+fi
 chmod +x "$CI_SCRIPTS/debian12.sh"
 
 # Creating cloudinit executable
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Downloading latest cloudinit to /usr/local/bin..."
+fi
 wget -O "/usr/local/bin/cloudinit" "https://raw.githubusercontent.com/ZaptoInc/cloudinit/refs/heads/master/cloudinit"
+if [[ "$CI_VERBOSE" -eq 1 ]]; then
+    echo "Making it executable"
+fi
 chmod +x "/usr/local/bin/cloudinit"
 
 echo "TODO"
--- variables.sh
+++ variables.sh
@@ -2,10 +2,19 @@
 CI_INSTALL_DIR="/var/lib/vz"
 export CI_INSTALL_DIR
 export CI_TEMPLATE_DIR="$CI_INSTALL_DIR/template/raw"
-export CI_SCRIPTS="$CI_INSTALL_DIR/cloudinit/scripts"
+CI_MAIN_DIR="$CI_INSTALL_DIR/cloudinit"
+export CI_MAIN_DIR
+export CI_SCRIPTS="$CI_MAIN_DIR/scripts"
 
 CI_CUSTOM_VARIABLES="/etc/cloudinit/custom.sh"
 if [[ -e $CI_CUSTOM_VARIABLES ]]; then
+    if [[ "$CI_VERBOSE" -eq 1 ]]; then
+        echo "Importing /etc/cloudinit/custom.sh"
+    fi
     chmod +x $CI_CUSTOM_VARIABLES
     source $CI_CUSTOM_VARIABLES
+else
+    if [[ "$CI_VERBOSE" -eq 1 ]]; then
+        echo "/etc/cloudinit/custom.sh not found, no custom variables imported"
+    fi
 fi
