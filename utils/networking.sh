71a6ca563be11eb112be475236bfe094712c1fc9
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2025-03-27 15:55:42 +0100

    now with first, last and usable

--- utils/networking.sh
+++ utils/networking.sh
@@ -7,7 +7,7 @@ ACTION="${ACTION^^}"
 
 # Vérifier le format
 if ! [[ "$RESEAU_CIDR" =~ ^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/(3[0-2]|[12]?[0-9])$ ]]; then
-  echo "Format invalide. Utilisation : $0 192.168.1.0/24"
+  echo "Format invalide. Utilisation : $0 192.168.1.0/24 [RANDOM|NETWORK|MASK|FIRST|LAST|USABLE]"
   exit 1
 fi
 
@@ -32,14 +32,21 @@ MASK=$(( 0xFFFFFFFF << (32 - CIDR) & 0xFFFFFFFF ))
 NET_ADDR=$(( NET_INT & MASK ))
 BROADCAST=$(( NET_ADDR | ~MASK & 0xFFFFFFFF ))
 
-if [[ $ACTION == "RANDOM" ]] then
-    # Générer une IP aléatoire entre NET_ADDR+1 et BROADCAST-1
-    RANDOM_IP=$(( RANDOM % (BROADCAST - NET_ADDR - 1) + NET_ADDR + 1 ))
-    int_to_ip "$RANDOM_IP"
-elif [[ $ACTION == "NETWORK" ]] then
-    int_to_ip "$NET_ADDR"
-elif [[ $ACTION == "MASK" ]] then
-    int_to_ip "$MASK"
+# Actions
+if [[ "$ACTION" == "RANDOM" ]]; then
+  RANDOM_IP=$(( RANDOM % (BROADCAST - NET_ADDR - 1) + NET_ADDR + 1 ))
+  int_to_ip "$RANDOM_IP"
+elif [[ "$ACTION" == "NETWORK" ]]; then
+  int_to_ip "$NET_ADDR"
+elif [[ "$ACTION" == "MASK" ]]; then
+  int_to_ip "$MASK"
+elif [[ "$ACTION" == "FIRST" ]]; then
+  int_to_ip $((NET_INT + 1))
+elif [[ "$ACTION" == "LAST" ]]; then
+  int_to_ip $((BROADCAST - 1))
+elif [[ "$ACTION" == "USABLE" ]]; then
+  echo $((BROADCAST - NET_INT - 1))
+else
+  echo "Action inconnue : $ACTION"
+  exit 1
 fi
-
-
