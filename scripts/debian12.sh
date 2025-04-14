f845f9005910c3c9102a086d661a221a343aa006
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2025-04-14 14:23:46 +0200

    testing script launch

--- scripts/debian12.sh
+++ scripts/debian12.sh
@@ -14,3 +14,16 @@ DownloadImage() {
 if [[ ! -e "$CI_TEMPLATE_DIR/$CI_IMAGE_NAME" || "$CI_UPDATE_OS" -eq 1 ]]; then
     DownloadImage
 fi
+
+echo "CI_VM_ID: $CI_VM_ID"
+echo "CI_VM_NAME: $CI_VM_NAME"
+echo "CI_OS_NAME: $CI_OS_NAME"
+echo "CI_OS_SCRIPT: $CI_OS_SCRIPT"
+echo "CI_CPU: $CI_CPU"
+echo "CI_RAM: $CI_RAM"
+echo "CI_RAM_MB: $CI_RAM_MB"
+echo "CI_DISK: $CI_DISK"
+echo "CI_CHOICE: $CI_CHOICE"
+echo "CI_NETWORK: $CI_NETWORK"
+echo "CI_CIDR: $CI_CIDR"
+echo "CI_IP_ADDRESS: $CI_IP_ADDRESS"
